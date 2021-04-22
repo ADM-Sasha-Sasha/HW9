f14c3468048e0e26f37ff80c4e5a649aaae44632
ADM-Sasha-Sasha <webi9876@gmail.com>
2021-04-22 17:53:29 +0300

    master

---
+++ src/main/java/Slova.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Slova {
+    private static final String path = "src/main/resources/words.txt";
+
+    public static void main(String[] args)
+    {
+        File file = new File(path);
+        checkFile(file);
+        readFile(file);
+    }
+
+    private static void readFile(File file)
+    {
+
+    }
+
+    private static void checkFile(File file)
+    {
+
+    }
+
+}
